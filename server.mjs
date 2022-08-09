f6cba1fddacdc7a40577fcb114c786daa32c8c69
sheraz <sherazzafar.148@gmail.com>
2022-08-09 09:47:33 +0500

    server

--- server.mjs
+++ server.mjs
@@ -1,6 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin:'*'
+}))
 const port = process.env.PORT || 3000
 let users = []
 
